1cebd13362cce4e9995958261b85c88aa00508b1
Evgeny <dikaevga@gmail.com>
2025-02-03 14:05:06 +0300

    Add files via upload

---
+++ build/entry.client-XQ5H5HAB.js
@@ -0,0 +1,1 @@
+import{a as _,f as r}from"/build/_shared/chunk-SIPEAGNB.js";import{P as e,b as y,d as t,e as E}from"/build/_shared/chunk-CUNT5ZIR.js";var n=y(o=>{"use strict";var i=_();o.createRoot=i.createRoot,o.hydrateRoot=i.hydrateRoot;var m});var a=t(E(),1),s=t(n(),1),c=t(e(),1);(0,a.startTransition)(()=>{(0,s.hydrateRoot)(document,(0,c.jsx)(r,{}))});
